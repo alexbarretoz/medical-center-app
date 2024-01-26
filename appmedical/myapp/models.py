a1b3f8b2b0028ffef02d15edbf583371c9b77112
alexbarretoz <alexbarreto729@gmail.com>
2024-01-26 12:58:09 -0500

    model

--- appmedical/myapp/models.py
+++ appmedical/myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.fields import CharField, URLField,IntegerField
 
 # Create your models here.
+
+class medicalCenter(models.Model):
+
+    id= IntegerField()
+    location= CharField(max_length=100)
+    idDoctor= IntegerField()
+
+
+
+class Doctor(models.Model)
+    id= IntegerField()
+    name= CharField(max_length=100)
+    especiality= CharField(max_length=100)
+
+
+
