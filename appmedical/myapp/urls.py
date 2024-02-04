19f73237c4595d5ed5f66ea0c7a2a1699db77ca7
alexbarretoz <alexbarreto729@gmail.com>
2024-02-04 12:02:38 -0500

    urls

--- appmedical/myapp/urls.py
+++ appmedical/myapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DoctorViewSet, ConsultationViewSet
+from .views import DoctorViewSet, ConsultationViewSet,medicalCenterViewSet, PatientViewSet
 
 router = DefaultRouter()
 router.register(r'doctor', DoctorViewSet, basename='doctor')
 router.register(r'consultation', ConsultationViewSet, basename='consultation')
-router.register(r'doctor', DoctorViewSet, basename='doctor')
-router.register(r'doctor', DoctorViewSet, basename='doctor')
+router.register(r'patient', PatientViewSet, basename='Patient')
+router.register(r'medicalcenter', medicalCenterViewSet, basename='medicalcenter')
 
 
 urlpatterns = [
