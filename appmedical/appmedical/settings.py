4db578faec589dfc3426a0be69128d0e0a7d16ea
alexbarretoz <alexbarreto729@gmail.com>
2024-02-26 12:47:04 -0500

    react

--- appmedical/appmedical/settings.py
+++ appmedical/appmedical/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # URL del frontend de React
+    "http://localhost:5173" # URL del frontend de React
 ]
